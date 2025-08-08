5aa06bf57fba32d28f9f0dae594212ffe42c2def
Surya Pradipta <suryapradipta982@gmail.com>
2025-08-08 22:10:11 +0700

    delete v1

--- src/store/hooks.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-
-import { AppDispatch, RootState } from './index'
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
